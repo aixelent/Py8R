435c7825f3984bfa77479c63292133651235f59d
xhexe <boulderdash@outlook.com>
2018-11-25 00:35:51 +0100

    Updated and added Py20 - Moved to sub directories and added second code sample

--- Py10/02/main1.py
+++ Py10/02/main1.py
@@ -1,11 +1,9 @@
-def calculate(x, y):
-    return (x * y) + (x * y) / 2**3
-
-
 x = float(input("Enter x: "))
 y = float(input("Enter y: "))
+
+doMath = (x * y) + (x * y) / 2**3
 
 if x and y == 0:
     print("0")
 else:
-    print("Tadam: ", calculate(x, y))
+    print("Result: ", doMath)
