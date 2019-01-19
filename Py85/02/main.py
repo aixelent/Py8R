fdf0cebb88bc46fb5f7cd9119102791086316458
xhexe <boulderdash@outlook.com>
2019-01-19 18:46:18 +0100

    Py85 - Added second sample

---
+++ Py85/02/main.py
@@ -0,0 +1,2 @@
+dict_values = {1: "C#", 2: "Cobol", 3: "Elixir", 4: "Golang", 5: "Haskell", 6: "Java", 7: "Javascript", 8: "Python", 9: "Scala"}
+print(dict_values.keys(), '\n', dict_values.values(), '\n', dict_values.items())
